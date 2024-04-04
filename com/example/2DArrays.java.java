c6d231e91c753e9e4b4848bcce4aeb2e5bafd6dd
Jaydin Madore <92885447+JaydinM@users.noreply.github.com>
2024-04-04 09:48:03 -0400

    Initial commit

---
+++ com/example/2DArrays.java.java
@@ -0,0 +1,28 @@
+
+package com.example;
+
+/**
+ * Change me.
+ *
+ * @author Jaydin Madore
+ * @version 1.0
+ * @since 2024-04-04
+ */
+
+// 2DArrays.java class
+public final class 2DArrays.java {
+
+    /** Private constructor to prevent instantiation. */
+    private 2DArrays.java() {
+        throw new UnsupportedOperationException("Cannot instantiate");
+    }
+
+    /**
+     * This is the main method.
+     *
+     * @param args Unused
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, world");
+    }
+}
